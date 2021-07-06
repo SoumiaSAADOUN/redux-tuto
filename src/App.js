0ba48099113774a81decd30c081fbf33cb9f2867
SoumiaSAADOUN <es_saadoun@esi.dz>
2021-07-06 13:16:38 +0100

    reducers and actions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useSelector, useDispatch} from 'react-redux'
+import {increment, decrement} from './actions/index'
 function App() {
+  const counter = useSelector(state => state.counter);
+  let logged = useSelector(state => state.logged);
+  const dispatcher= useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <h1> Counter {counter} </h1>
+         <button onClick={()=>{dispatcher(increment())}}>+</button>
+         <button onClick={()=> {dispatcher(decrement())}}>-</button>
+         {logged? <h2>you can see this because you're logged in</h2>: <h1>You've to login</h1>}
+
     </div>
   );
 }
